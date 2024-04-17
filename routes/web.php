1ceece45745c602b74a05fb7f61aefe2523f2b3e
Arif <md.arif.tiens@gmail.com>
2024-04-18 00:07:46 +0600

    added middleware config in route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('history-keeper')->group( function (){
+Route::prefix('history-keeper')->middleware(config("historyKeeper.middleware"))->group( function (){
     Route::get('/', [\CodeStepsBD\HistoryKeeper\Controllers\HistoryKeeperController::class,'index']);
 });
